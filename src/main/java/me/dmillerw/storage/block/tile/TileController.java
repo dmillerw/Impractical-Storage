8c8b8af4e5ea72941af74070d247e1560fd9363c
dmillerw <dmillerw@gmail.com>
2017-03-31 22:44:16 -0700

    Wait to place blocks in world until the entire itemstack has been inserted. 1.10 version of #28

--- src/main/java/me/dmillerw/storage/block/tile/TileController.java
+++ src/main/java/me/dmillerw/storage/block/tile/TileController.java
@@ -386,17 +386,22 @@ public class TileController extends TileCore implements ITickable {
                 shouldShiftInventory = false;
             }
 
-            if (CommonProxy.useBlockQueue()) {
-                blockQueueTickCounter++;
+            blockQueueTickCounter++;
 
-                if (blockQueueTickCounter >= CommonProxy.blockUpdateRate) {
+            if (CommonProxy.blockUpdateRate == -1 || blockQueueTickCounter >= CommonProxy.blockUpdateRate) {
+                if (CommonProxy.blockUpdateBatch == -1) {
+                    for (int i = 0; i < blockQueue.size(); i++) {
+                        QueueElement element = blockQueue.pop();
+                        setBlock(element.slot, element.itemStack);
+                    }
+                } else {
                     for (int i = 0; i < Math.min(blockQueue.size(), CommonProxy.blockUpdateBatch); i++) {
                         QueueElement element = blockQueue.pop();
                         setBlock(element.slot, element.itemStack);
                     }
-
-                    blockQueueTickCounter = 0;
                 }
+
+                blockQueueTickCounter = 0;
             }
 
             if (worldObj.getTotalWorldTime() % 10 == 0) {
@@ -629,18 +634,14 @@ public class TileController extends TileCore implements ITickable {
 
         if (itemStack == null || itemStack.stackSize <= 0) {
             shouldShiftInventory = true;
-        } else {
-            if (CommonProxy.useBlockQueue()) {
-                blockQueueTickCounter = 0;
-
-                QueueElement element = new QueueElement();
-                element.slot = slot;
-                element.itemStack = itemStack;
-                blockQueue.add(element);
-            } else {
-                setBlock(slot, itemStack);
-            }
         }
+
+        blockQueueTickCounter = 0;
+
+        QueueElement element = new QueueElement();
+        element.slot = slot;
+        element.itemStack = itemStack;
+        blockQueue.add(element);
     }
 
     private void shiftInventory() {
