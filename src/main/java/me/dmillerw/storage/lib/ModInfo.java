7d2c3f80676e29e4efd8380c2ca75381aca766c2
dmillerw <dmillerw@gmail.com>
2017-04-07 23:59:55 -0700

    Fixed gradle things + setup update tracker

--- src/main/java/me/dmillerw/storage/lib/ModInfo.java
+++ src/main/java/me/dmillerw/storage/lib/ModInfo.java
@@ -8,4 +8,5 @@ public class ModInfo {
     public static final String ID = "impstorage";
     public static final String NAME = "Impractical Storage";
     public static final String VERSION = "%MOD_VERSION%";
+    public static final String UPDATE_JSON = "%VERSION_JSON%";
 }
